94aab916434fae1c5a044184661f5004b883c4d9
pratiksj <pratiksharai728@gmail.com>
2023-01-30 22:32:09 +0545

    feat:13:13 made search work case sensitive

--- controllers/blog.js
+++ controllers/blog.js
@@ -29,7 +29,7 @@ router.get("/", async (req, res) => {
 
   const where = {};
   if (req.query.search) {
-    where.title = { [Op.substring]: req.query.search };
+    where.title = { [Op.iLike]: `%${req.query.search}%` };
   }
   const blogs = await Blog.findAll({
     attributes: { exclude: ["userID"] },
